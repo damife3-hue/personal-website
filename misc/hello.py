3b408879c36e39b6497e0a15d5b499ce7ca073c4
Damife Odunukan <Damife3@gmail.com>
2026-09-26 12:51:50 +0100

    for loop in python

--- misc/hello.py
+++ misc/hello.py
@@ -1,1 +1,9 @@
-print ("Hello from python!")
+for i in range(1,51):
+    if i % 15 == 0: #we use 15 because it is the lowest multiple of 3 and 5. this also works: i % 3 == 0 and i % 5 == 0: 
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
